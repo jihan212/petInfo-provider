90b68335b112b0e10d0d6bfb9b8ec4d2a493dbd7
snapper <zihanjashim2121@gmail.comm>
2022-05-22 00:08:58 +0600

    first commit

---
+++ front-end/src/Components/Home/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='navbar bg-light'>
+      <div className='container-fluid'>
+        <a
+          className='navbar-brand mb-0 fs-3 fst-italic fw-bold text-reset'
+          style={{ textDecoration: 'none' }}
+          href='/'
+        >
+          petInfo
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
