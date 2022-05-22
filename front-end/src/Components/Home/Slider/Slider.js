ff5b4ecefacc3cf1be52f1146784dcb5d5117fb9
snapper <zihanjashim2121@gmail.comm>
2022-05-23 00:16:34 +0600

    homepage updated

---
+++ front-end/src/Components/Home/Slider/Slider.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './slider.css'
+
+function Slider() {
+
+  return (
+    <section className='textCenter'>
+      <h1 class='text-center text-uppercase fs-1 fw-bolder heading'>
+        Welcome to my site
+      </h1>
+      <p class='text-center paragraph'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
+        voluptates beatae voluptas doloremque. Dolorum inventore dolorem laborum
+        molestias, suscipit provident optio illo ab! Eius sit dicta quibusdam
+        odio earum eos!
+      </p>
+    </section>
+  )
+}
+
+export default Slider
